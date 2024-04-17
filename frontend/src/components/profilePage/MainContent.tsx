ecd25a5ce8f768047ccb743dded455834ba3fe59
lorenzo-cm <lorenzocarneirobr@gmail.com>
2024-04-17 09:40:56 -0300

    debug in chatArea and uploadArea started

--- frontend/src/components/profilePage/MainContent.tsx
+++ frontend/src/components/profilePage/MainContent.tsx
@@ -1,10 +1,40 @@
-// ../components/profilePage/MainContent.tsx
-import React from "react";
+import React, { useEffect } from "react";
+import { User } from "../../models/user";
+
+import UploadSection from "./uploadSection";
 
 interface MainContentProps {
   section: string;
+  user: User|undefined;
 }
 
-export const MainContent: React.FC<MainContentProps> = ({ section }) => {
-  return <div>{/* Render content based on the section */}</div>;
+interface UserProfileProps {
+  user: User;
+}
+
+const UserProfile: React.FC<UserProfileProps> = ({ user }) => (
+  <div>
+    <h1 className="font-bold text-xl mb-2">User Profile</h1>
+    <p><strong>Name:</strong> {user.username}</p>
+    <p><strong>Email:</strong> {user.email}</p>
+    <p><strong>Role:</strong> {user.role}</p>
+  </div>
+);
+
+// const UploadSection = () => <div>Upload Section</div>;
+
+export const MainContent: React.FC<MainContentProps> = ({ section, user }) => {
+  let content;
+  switch (section) {
+    case 'user':
+      content = user ? <UserProfile user={user} /> : <div>Loading...</div>;
+      break;
+    case 'upload':
+      content = <UploadSection />;
+      break;
+    default:
+      content = <div>Section not found</div>;
+  }
+
+  return <div className="p-4">{content}</div>;
 };
