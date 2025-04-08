28d68107aa1f416fd0b3ad54359333445b19a879
Lorenzo Carneiro <lorenzocarneirobr@gmail.com>
2025-04-08 17:02:19 -0300

    2962

--- lista01/2962.cpp
+++ lista01/2962.cpp
@@ -25,6 +25,7 @@ class Sensor{
             this->x = x_;
             this->y = y_;
         }
+        Sensor(){};
 };
 
 int find(vector<int> &union_find, int x){
@@ -55,24 +56,28 @@ bool left_border(int x, int y, int r){
 }
 
 bool right_border(int x, int y, int r){
-    return x+r >= n;
+    return x+r >= m;
 }
 
 bool top_border(int x, int y, int r){
-    return  y+r >= m;
+    return  y+r >= n;
 }
 
 bool bottom_border(int x, int y, int r){
     return  y-r <= 0;
 }
 
+bool sensors_intercept(Sensor &s1, Sensor &s2){
+    return (pow((s1.x-s2.x), 2) + pow((s1.y-s2.y), 2)) <= (s1.r + s2.r)*(s1.r + s2.r);
+}
 
 int main(){ _
 
     cin >> m >> n >> k;
     int tx, ty, tr;
 
-    vector<int> union_find(k+4, -1);
+    vector<int> union_find(k+4);
+    for(int i = 0; i<k+4; i++) union_find[i] = i;
     vector<Sensor> sensors(k);
 
      
@@ -85,19 +90,22 @@ int main(){ _
         if(top_border(tx, ty, tr))  union_func(union_find, 1, i+4);
         if(right_border(tx, ty, tr))  union_func(union_find, 2, i+4);
         if(bottom_border(tx, ty, tr))  union_func(union_find, 3, i+4);
-
-        union_find.push_back({i, });
-
-
     }
 
     for(int i=0; i<k; i++){
-        for(int j=0; j<k; j++){
-
+        for(int j=i+1; j<k; j++){
+            if(sensors_intercept(sensors[i], sensors[j])) union_func(union_find, i+4, j+4);
         }
     }
 
+    if(find(union_find, 0) == find(union_find, 2) or 
+       find(union_find, 0) == find(union_find, 3) or
+       find(union_find, 1) == find(union_find, 3) or
+       find(union_find, 1) == find(union_find, 2))
+        cout << "N" << endl;
 
+    else
+        cout << "S" << endl;
     
     return 0;
 }
