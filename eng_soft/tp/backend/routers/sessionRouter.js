599eb5c28258cb547036488e46680f554336c30d
Lorenzo Carneiro Magalhães <lorenzocarneirobr@gmail.com>
2026-02-18 10:03:07 -0300

    remove to add later

--- eng_soft/tp/backend/routers/sessionRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-import { authSession } from '../session/sessionManager.js';
-
-const router = express.Router();
-
-router.get('/check', authSession);
-
-export default router;
